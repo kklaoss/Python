9fd3e901f341951e0f30fa3b2c2c428f0143d75c
klaos <klaos.usn@gmail.com>
2024-12-08 22:08:22 +0300

    remake task_2.py

--- task_2.py
+++ task_2.py
@@ -6,13 +6,9 @@ temperature_limit = -27
 age_limit = 9
 if holiday == 1:
     print('\nИдем к друзьям')
+elif int(temperature) > temperature_limit:
+    print('\nВ школу :(')
+elif int(age) < age_limit:
+    print('\nСидим дома :)')
 else:
-    print('\nНе идем к друзьям')
-if int(temperature) > temperature_limit:
-    print('В школу...?')
-else:
-    print('Не в школу...?')
-if int(age) < age_limit:
-    print('Сидим дома :)')
-else:
-    print('Тащимся в школу :(')
+    print('\nТащимся в школу :(')
